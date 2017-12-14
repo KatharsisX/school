25180d1c579b8e5f79d9fabbe6a193dd3779d691
Przemek <przemyslaw.bochna@gmail.com>
2017-12-14 07:56:25 +0000

    updates

--- Contacts-databases/connect.php
+++ Contacts-databases/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$server='userdb1';
-$user='1141812_AgA';
-$pass='4PiI2dax1HdyKJ';
-$db='1141812_AgA';
+$server='localhost';
+$user='katharsisx';
+$pass='';
+$db='Kontakty';
 
 $mysqli = new mysqli($server,$user,$pass,$db);
