d9db9ee38f1e74c7e5bbe84fd743d25098539924
Przemek <przemyslaw.bochna@gmail.com>
2017-12-20 10:17:05 +0000

    zdjecia

--- Contacts-databases/connect.php
+++ Contacts-databases/connect.php
@@ -1,8 +1,9 @@
 <?php
 
-$server='localhost';
-$user='katharsisx';
-$pass='';
-$db='Kontakty';
+$server='poligon.zsp2.krotoszyn.net';
+$user='4f_bochna_pr';
+$pass='98013108871';
+$db='4f_bochna_pr_kontakty';
+$port='33060';
 
-$mysqli = new mysqli($server,$user,$pass,$db);
+$mysqli = new mysqli($server,$user,$pass,$db, $port);
